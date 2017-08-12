16062019373f48f32df744c275213b971e957c2a
Elizabeth Niebauer <egniebauer@gmail.com>
2017-08-12 13:40:40 -0500

    load and display NY Times articles

--- js/script.js
+++ js/script.js
@@ -17,12 +17,40 @@ function loadData() {
     var address = streetStr + ', ' + cityStr;
 
     // display greeting
-    $('#greeting').text('So you want to live at ' + address + '?');
+    $greeting.text('So you want to live at ' + address + '?');
 
     // load streetview
     var imgSrc = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address;
     var backgroundImg = '<img class="bgimg" src="' + imgSrc + '">';
     $('body').append(backgroundImg);
+
+    // nytimes header
+    $nytHeaderElem.text('New York Times Articles for ' + address);
+
+    // nytimes articles
+    var apiKey = 'c5a889f96706482984d1c5d640e5228a';
+    var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + address +'&sort=newest&api-key=' + apiKey;
+
+    $.getJSON(url, function( data ) {
+      $nytElem.empty();
+
+      var nytArticles = [];
+      var docs = data.response.docs;
+
+      for (var i in docs) {
+
+        var webUrl = docs[i].web_url;
+        var printHeadline = docs[i].headline.print_headline;
+        var snippet = docs[i].snippet;
+
+        var article = '<li class="article"><a href="' + webUrl + '">' + printHeadline + '</a><p>' + snippet + '</p></li>';
+        nytArticles.push( article );
+      }
+
+      $nytElem.append(nytArticles.join( "" ));
+  });
+
+    // wikipedia
 
 
     return false;
