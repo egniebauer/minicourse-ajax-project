35d7552a956edd062b7bbefa36aa12834ec72fbf
Elizabeth Niebauer <egniebauer@gmail.com>
2017-08-13 14:02:26 -0500

    implement Relevent Wikipedia Links with error handling

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function loadData() {
     var address = streetStr + ', ' + cityStr;
 
     // display greeting
-    $greeting.text('So you want to live at ' + address + '?');
+    $greeting.text('So you want to live in ' + cityStr + '?');
 
     // load streetview
     var imgSrc = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address;
@@ -25,16 +25,15 @@ function loadData() {
     $('body').append(backgroundImg);
 
     // nytimes header
-    $nytHeaderElem.text('New York Times Articles for ' + address);
+    $nytHeaderElem.text('New York Times Articles for ' + cityStr);
 
     // nytimes articles
     var apiKey = 'c5a889f96706482984d1c5d640e5228a';
-    var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + address +'&sort=newest&api-key=' + apiKey;
+    var nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr +'&sort=newest&api-key=' + apiKey;
 
-    $.getJSON(url, function( data ) {
+    $.getJSON(nytUrl, function( data ) {
       $nytElem.empty();
 
-      var nytArticles = [];
       var docs = data.response.docs;
 
       for (var i in docs) {
@@ -44,16 +43,36 @@ function loadData() {
         var snippet = docs[i].snippet;
 
         var article = '<li class="article"><a href="' + webUrl + '">' + printHeadline + '</a><p>' + snippet + '</p></li>';
-        nytArticles.push( article );
+        $nytElem.append(article);
       }
 
-      $nytElem.append(nytArticles.join( "" ));
-  }).fail(function() {
-    $nytHeaderElem.text('New York Times Articles Could Not Load');
-  });
+    }).fail(function(e) {
+      $nytHeaderElem.text('New York Times Articles Could Not Load');
+    });
 
     // wikipedia
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + cityStr;
 
+    var wikiTimeout = setTimeout( function(){
+      $wikiElem.text('Wikipedia Links Could Not Load');
+    }, 8000 );
+
+    $.ajax({
+      url : wikiUrl,
+      dataType : "jsonp"})
+      .done( function( response ) {
+        console.log( response );
+
+        var links = response[1];
+
+        for (var i in links) {
+          var page = links[i];
+          var link = '<li><a href="https://en.wikipedia.org/wiki/' + page + '">' + page + '</a></li>';
+          $wikiElem.append( link );
+        }
+
+        wikiTimeout = clearTimeout(wikiTimeout);
+      });
 
     return false;
 };
